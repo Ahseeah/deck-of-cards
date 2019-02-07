e9ff7cc36669420c88fd69b1eb824d5fd640ffec
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-02-07 16:46:26 -0500

    Added shuffle and drawing a card

--- public/main.js
+++ public/main.js
@@ -1,168 +1,70 @@
-const main = () => {
-  let suits = [‘hearts’, ‘diamonds’, ‘clubs’, ‘spades’]
-  let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, ‘Jack’, ‘Queen’, ‘King’, ‘Ace’]
- 
-  let deck = []
- 
-  // make a function that creates a deck
-  const makeDeck = () => {
-    //deck = []
- 
-    // for each type of suit
-    for (let i = 0; i < suits.length; i++) {
-      // and for each rank
-      for (let j = 0; j < ranks.length; j++) {
-        // make a card
-        let card = {}
-        card.suit = suits[i]
-        card.rank = ranks[j]
- 
-        deck.push(card)
-      }
-    }
-    console.log(deck)
-  }
- 
-  const shuffleDeck = () => {
-    // Shuffle the deck
-  }
- 
-  const drawCard = () => {
-    if (deck.length > 0) {
-      let card = deck.shift()
-      console.log(card)
- 
-      // put the card on the screen
-      // - first find the “deck” div
-      const element = document.querySelector(‘.deck’)
-      console.log(element)
-      // - create elelment
-      // - set textContent and classes
-      // - append to the element
-    }
-  }
- 
-  makeDeck()
-  shuffleDeck()
-  drawCard()
- 
-  // let randIndex = Math.floor(Math.random() * deck.length)
-  // $(‘#drawbtn’).click(function() {
-  //   let myCard = drawCard()
- 
-  //   if (myCard) {
-  //     makeCard(myCard.suit, myCard.rank)
-  //   } else {
-  //     alert(‘no more cards in the deck’)
-  //   }
-  // })
- 
-  // console.log(‘Deck of Cards ‘, deck.length, ’ CARDS’)
-  // console.log(deck)
- }
- 
- // function makeCard(suit, rank) {
- //   let card = $(‘.card.template’).clone()
- 
- //   card.removeClass(‘template’)
- 
- //   card.find(‘.rank’).html(rank)
- //   card.find(‘.suit’).html(‘&’ + suit + ‘;’)
- 
- //   if (suit === ‘hearts’ || suit === ‘diams’) {
- //     card.addClass(‘red’)
- //   }
- 
- //   $(‘body’).append(card)
- // }
- 
- document.addEventListener(‘DOMContentLoaded’, console.log('testing'))
- 
- // function newFunction(makeDeck) {
- //   makeDeck()
- // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let cardSelection;
-const cardSelectionClubs=() => {
-  cardSelection= 'Clubs'
-  console.log(cardSelection)
-  document.querySelector('card-pick').textContent= cardSelection
-}
-
-let cards = [
-'Ace',
-'2' ,
-'3' ,
-'4' ,
-'5' ,
-'6' ,
-'7' ,
-'8' ,
-'9' ,
-'10' ,
-'Jack' ,
-'Queen' ,
-'King' ,
+let suits = ['hearts', 'diamonds', 'clubs', 'spades']
+let ranks = [
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'Jack',
+  'Queen',
+  'King',
+  'Ace'
 ]
 
-let suit = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+let deck = []
 
-const buildDeck= () =>{
-for cardSelection (let i;)
+// make a function that creates a deck
+const makeDeck = () => {
+  // for each type of suit
+  for (let i = 0; i < suits.length; i++) {
+    // and for each rank
+    for (let j = 0; j < ranks.length; j++) {
+      // make a card
+      let card = suits[i] + ' of ' + ranks[j]
+      deck.push(card)
+    }
+  }
+  console.log(deck)
 }
 
-const shuffleDeck=()=> {
-
+const shuffleDeck = () => {
+  for (let i = deck.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const cardJ = deck[j]
+    const cardI = deck[i]
+    deck[j] = cardI
+    deck[i] = cardJ
+  }
+  // Shuffle the deck
 }
 
-const dealCard= (event) => {
+const drawCard = () => {
+  let card = deck.shift()
+  if (deck.length > 0) {
+    const _li = document.createElement('li')
+    _li.textContent = card
+    document.querySelector('ul').appendChild(_li)
+    console.log(card)
 
+    // put the card on the screen
+    // - first find the “deck” div
+    // - create elelment
+    // - set textContent and classes
+    // - append to the element
+  }
 }
 
 const main = () => {
-  buildDeck ()
-  shuffleDeck ()
-  //Deal cards in deck
-  const button =document.querySelector('button')
-  button.addEventListener('click, dealCard')
-
-
-  if (document.querySelector('h1.deck-of-cards')) {
-    document.querySelector('h1.deck-of-cards').textContent = 'Do the Shuffle!'
-  }
-
-  document.addEventListener('DOMContentLoaded', main)
-
-  document
-    .querySelector('#clubsButton')
-    .addEventListener('click', playerSelectedClubs)
-    .querySelector('#diamondsButton')
-    .addEventListener('click', playerSelectedDiamonds)
-    .querySelector('#heartsButton')
-    .addEventListener('click', playerSelectedHearts)
-    .querySelector('#spadesButton')
-    .addEventListener('click', playerSelectedSpades)
-
-
-
-
-  
-  /*function random_bg_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
- console.log(bgColor);
+  makeDeck()
+  shuffleDeck()
+  console.log(deck)
+  const button = document.querySelector('.dealCard')
+  button.addEventListener('click', drawCard)
+  // drawCard()
 }
+
+document.addEventListener('DOMContentLoaded', main)
